6270b22f089fa7cbd74601777d1ec23973874cf8
polvoras <boibatorta@gmail.com>
2026-05-24 05:07:55 +0100

    fix: add explicit jsEngine hermes, add unstable_transformImportMeta, remove expo-build-properties

--- mobile-classic/babel.config.js
+++ mobile-classic/babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
     plugins: [
       "@babel/plugin-transform-class-properties",
       "@babel/plugin-transform-private-methods",
